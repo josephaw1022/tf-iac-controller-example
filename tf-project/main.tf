90b4f665390cea04b29a0d231c53be30fe295192
josephaw1022 <josephsims1@gmail.com>
2025-04-27 14:54:35 -0400

    remove error causing property

--- tf-project/main.tf
+++ tf-project/main.tf
@@ -3,11 +3,8 @@
 # Just write the terraform code and it will be applied automatically once merged to main
 
 
-
-
 provider "aws" {
   region                      = "" # picked up from AWS_REGION env
-  s3_force_path_style         = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
 }
