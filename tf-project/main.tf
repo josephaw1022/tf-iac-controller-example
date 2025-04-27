5596b3db4aff43ea8c64c5cbb232ac476208a96c
josephaw1022 <josephsims1@gmail.com>
2025-04-27 15:51:40 -0400

    modify tf

--- tf-project/main.tf
+++ tf-project/main.tf
@@ -3,10 +3,25 @@
 # Just write the terraform code and it will be applied automatically once merged to main
 
 
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
   region                      = "" # picked up from AWS_REGION env
   skip_credentials_validation = true
   skip_metadata_api_check     = true
+  skip_requesting_account_id = true
+
+  access_key     = "test"
+  secret_key     = "test"
+  region       = "us-east-1"
 }
 
 resource "aws_s3_bucket" "example" {
