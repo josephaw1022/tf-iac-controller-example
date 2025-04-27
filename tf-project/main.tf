e41c24146d8e5db67626f636382d4598901a79b8
josephaw1022 <josephsims1@gmail.com>
2025-04-27 16:08:45 -0400

    fix err in file

--- tf-project/main.tf
+++ tf-project/main.tf
@@ -14,14 +14,13 @@ terraform {
 }
 
 provider "aws" {
-  region                      = "" # picked up from AWS_REGION env
   skip_credentials_validation = true
   skip_metadata_api_check     = true
-  skip_requesting_account_id = true
+  skip_requesting_account_id  = true
 
-  access_key     = "test"
-  secret_key     = "test"
-  region       = "us-east-1"
+  access_key = "test"
+  secret_key = "test"
+  region     = "us-east-1"
 }
 
 resource "aws_s3_bucket" "example" {
