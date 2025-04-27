8fd0c2693f97a5dd5c0fcfe542d95343f0bb47bb
josephaw1022 <josephsims1@gmail.com>
2025-04-27 16:21:46 -0400

    fix path style

--- tf-project/main.tf
+++ tf-project/main.tf
@@ -17,6 +17,8 @@ provider "aws" {
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
+  s3_use_path_style           = false
+
 
   access_key = "test"
   secret_key = "test"
